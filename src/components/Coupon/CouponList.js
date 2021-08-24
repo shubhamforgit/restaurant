707f3f426f93045bce45be4df7aea95e34a63413
shubhamforgit <shubhamforgit@gmail.com>
2021-08-24 17:51:21 +0530

    added err handling

--- src/components/Coupon/CouponList.js
+++ src/components/Coupon/CouponList.js
@@ -1,18 +1,24 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { CardGroup, Spinner } from "react-bootstrap"
+import { CardGroup, Spinner, Alert } from "react-bootstrap"
 import Coupon from "./Coupon"
 
 const CouponList = () => {
 
     const [isLoading, setIsLoading] = useState(true)
     const [coupons, setCoupons] = useState([])
+    const [errorOccured, setErrorOccured] = useState(false)
+
 
     useEffect(() => {
         axios.get("https://food-app-timesinternet.herokuapp.com/api/staff/coupon")
             .then(response => {
                 setCoupons(response.data)
                 setIsLoading(false)
+            })
+            .catch((err) => {
+                setIsLoading(false)
+                setErrorOccured(true)
             })
     }, [])
 
@@ -22,6 +28,10 @@ const CouponList = () => {
                 <span className="visually-hidden">Loading...</span>
             </Spinner>
         )
+    } else if (errorOccured) {
+        return <Alert variant="danger">
+            Error Occured! (Get Request Failed)
+        </Alert>
     } else {
         return (
             <CardGroup style={{ justifyContent: "center" }}>
